15e5162d0db094a0ba72bc6127744e8af0732a9d
Khuthadzo TSHIKOTSHI <ktshikot@e5r1p2.jnb.42.fr>
2016-10-15 13:24:49 +0200

    aaaa

--- includes/expert.class.php
+++ includes/expert.class.php
@@ -2,65 +2,137 @@
 
 class expert
 {
-public $facts;
-public $query;
-public $rules = array();
-
-public function __construct($file)
-{
-	foreach ($file as $line)
-		$this->read_line($line);
+	public $facts;
+	public $query;
+	public $rules = array();
+	public $nl;
+	public function __construct($file, $argnl)
+	{
+		foreach ($file as $line)
+			$this->read_line($line);
 		$this->change_rules();
-}
-
-public function getvars()
-{
-	echo $this->facts."<br>";
-	echo $this->query."<br>";
-	foreach ($this->rules as $i)
-	echo $i."<br>";
-}
-
-private function read_line($line)
-{
-	$line = trim($line);
-	if($line[0] != '#')
-	{
-	if(preg_match('/^(.*?)#/',$line, $ret))
-	{
-		$ret = preg_replace("/#/" , "\0" , $ret);
-		$ret[0] =  preg_replace('/\s\s+/', '', $ret[0]);
-		$ret[0] = str_replace(' ', '', $ret[0]);
-		$this->show($ret[0]);
+		$this->nl = $argnl;
 	}
-	else
-	{
-		$line =  preg_replace('/\s\s+/', '', $line);
-		$line = str_replace(' ', '' , $line);
-		$this->show($line);
-	}
-	}
-}
 
-private function show($line)
-{
-	if($line[0] === '=')
-		$this->facts = $line;
-	else if($line[0] === '?')
-		$this->query = $line;
-	else if (!empty($line)) 
-		array_push($this->rules, $line);
-}
-
-public function change_rules()
-{
-	$i = 0;
-	while ($this->rules[$i])
+	public function getvars()
 	{
-		$this->rules[$i] = preg_replace('/=>/' , '=', $this->rules[$i]);
-		$this->rules[$i] = preg_replace('/<=/' , '_', $this->rules[$i]);
-		$i++;
+		foreach ($this->rules as $i)
+		{
+			if((strchr($i, '=') or strchr($i, '_')) && (ctype_alpha($i[0])
+			or $i[0] === '(' or $i[0] === '!' ) && $this->check_syntax($i) === 0)
+			$this->print_i($i);
+			else
+				echo "Syntax Error".$this->nl;
+		}
+			echo $this->nl.$this->facts.$this->nl;
+		echo $this->query.$this->nl;
 	}
-}
+	
+	private function check_syntax($line)
+	{
+		$i = 1;
+		while($line[$i])
+		{
+			$c = $line[$i];
+			if($c === '=' or $c ==='_' or $c === '+' or 
+			$c === '|' or $c === '^')
+			{
+				if(!ctype_alpha($line[$i + 1]))
+				{
+					if($line[$i + 1] !== '!' && $line[$i + 1] !== '(')
+					return 1;
+				}
+				if(!ctype_alpha($line[$i - 1]))
+				{
+					if($line[$i - 1] !== ')')
+					return 1;
+				}
+			}
+			else if($c === '(')
+			{
+				if(ctype_alpha($line[$i - 1]))
+					return 1; 
+			}
+			else if($c === ')')
+			{
+				if(ctype_alpha($line[$i + 1]) or $line[$i + 1] === '!')
+					return 1;
+			}
+			else if(ctype_alpha($c))
+			{
+				if(ctype_alpha($line[$i + 1]) or ctype_alpha($line[$i - 1])) 
+					return 1;
+			}
+			else if ($c === '!')
+			{
+				if($line[$i - 1] === '!')
+					return 1;
+				if(!ctype_alpha($line[$i + 1]))
+					return 1;
+			} 
+			else
+				return 1;
+			$i++;
+		}
+		return 0;
+	}
+
+	private function read_line($line)
+	{
+		$line = trim($line);
+		if($line[0] != '#')
+		{
+			if(preg_match('/^(.*?)#/',$line, $ret))
+			{
+				$ret = preg_replace("/#/" , "\0" , $ret);
+				$ret[0] =  preg_replace('/\s\s+/', '', $ret[0]);
+				$ret[0] = str_replace(' ', '', $ret[0]);
+				$this->show($ret[0]);
+			}
+			else
+			{
+				$line =  preg_replace('/\s\s+/', '', $line);
+				$line = str_replace(' ', '' , $line);
+				$this->show($line);
+			}
+		}
+	}
+	
+	public function print_i($str)
+	{	
+		$i = 0;
+		while($str[$i])
+		{
+			if($str[$i] === '=' )
+				echo '=>';
+			else if ($str[$i] === '_')
+				echo '<=>';
+			else 
+				echo $str[$i];
+			$i++;
+		}
+		echo $this->nl;	
+	}
+
+	private function show($line)
+	{
+		if($line[0] === '=')
+			$this->facts = $line;
+		else if($line[0] === '?')
+			$this->query = $line;
+		else if (!empty($line)) 
+			array_push($this->rules, $line);
+	}
+
+	public function change_rules()
+	{
+		$i = 0;
+		while ($this->rules[$i])
+		{
+			$this->rules[$i] = preg_replace('/=>/' , '=', $this->rules[$i]);
+			$this->rules[$i] = preg_replace('/<=/' , '_', $this->rules[$i]);
+			$i++;
+		}
+	}
 }
 ?>
--- includes/run.class.php
+++ includes/run.class.php
@@ -4,15 +4,17 @@ include_once('expert.class.php');
 class run extends expert
 {
 	public $alpha = array('0'=> 0, '1' => 1, 'A' => 0 , 'B' => 0 , 'C' => 0 , 'D' => 0 , 'E' => 0 , 'F' => 0,
-		'G' => 0 , 'H' => 0 , 'I' => 0 , 'J' => 0 , 'K' => 0 , 'L' => 0 , 'M' => 0 , 
-		'N' => 0 , 'O' => 0,'P' => 0 , 'Q' => 0 , 'R' => 0 , 'S' => 0 , 'T' => 0 , 
-		'U' => 0 , 'V' => 0 , 'W' => 0 , 'X' => 0, 'Y' => 0 , 'Z' => 0);
+			'G' => 0 , 'H' => 0 , 'I' => 0 , 'J' => 0 , 'K' => 0 , 'L' => 0 , 'M' => 0 , 
+			'N' => 0 , 'O' => 0,'P' => 0 , 'Q' => 0 , 'R' => 0 , 'S' => 0 , 'T' => 0 , 
+			'U' => 0 , 'V' => 0 , 'W' => 0 , 'X' => 0, 'Y' => 0 , 'Z' => 0);
 	public $left = array();
 	public $right = array();
+	public $imp = array();
+	public	$chk = array();
 
-	public function __construct($file)
+	public function __construct($file , $argnl)
 	{
-		parent::__construct($file);
+		parent::__construct($file, $argnl);
 		$this->assign();
 	}
 
@@ -20,7 +22,7 @@ class run extends expert
 	{
 		foreach ($this->alpha as $elem => $line)
 		{
-			echo $elem.":".$line."<br>";
+			echo $elem.":".$line.$this->nl;
 		}
 	}
 
@@ -37,7 +39,7 @@ class run extends expert
 					echo "True";
 				else
 					echo "False";
-				echo "<BR>";
+				echo $this->nl;
 			}
 			$i++;
 		}	
@@ -46,7 +48,7 @@ class run extends expert
 	public function test()
 	{
 		$this->getvars();
-		echo "-------------------------------<BR>";
+		echo "-------------------------------".$this->nl;
 		$this->recur_();
 		$this->display();
 	}
@@ -54,31 +56,47 @@ class run extends expert
 	private function recur_()
 	{
 		$this->change_();
-		$chk = array();
-		$chk = array_combine($this->right , $this->left);	
-		foreach($chk as $lft => $rht)
+		$this->chk = array();
+		$this->chk = array_combine($this->right , $this->left);	
+		foreach($this->chk as $rht => $lft)
 		{
-			$a = $rht;
-			$b = $lft;
+			$a = $lft;
+			$b = $rht;
 			if($a !== $this->alpha[$b])
 			{
 				$this->alpha[$b] = $a;
 				$this->recur_();
 			}
-
-		}
-		//	 print_r ($this->left);
+			else if(strlen($b) > 2)
+			{
+				if($this->solver_r($b, $a) > 0)
+					$this->recur_();
+			}
+		}	
 	}
 
 	private function change_()
 	{	
 		$this->left = array();
 		$this->right = array();	
+		$this->imp = array();
 		foreach($this->rules as $rule)
 		{
+
+			if(strpos($rule, '=') !== false)
+				array_push($this->imp, '=');				
+			else if (strpos($rule, '_') !== false)
+				array_push($this->imp, '_');
+
 			$split = explode("=", $rule);
 			$line = $split[0];
 			array_push($this->right, $split[1]);
+
+			if(preg_match_all('/\((.*?)\)/', $line, $match))
+			{
+				foreach ($match[0] as $i)
+					$line = str_replace($i,$this->solver_($i),$line);
+			}	
 			if(!$line[1])
 				array_push($this->left,$this->alpha[$line[0]]);
 			else
@@ -101,16 +119,18 @@ class run extends expert
 		}	
 	}
 
-	function solver_($line)
+	private function solver_($line)
 	{
 		$k = 0;
-		while($k < 3)
+		while($k < 4)
 		{
 			if($k === 0)
+				$chk = '!';
+			else if($k === 1)
 				$chk = '+';
-			else if ($k === 1)
-				$chk = '|';
 			else if ($k === 2)
+				$chk = '|';
+			else if ($k === 3)
 				$chk = '^';
 			$i = 0;	
 			while($line[$i])
@@ -121,8 +141,9 @@ class run extends expert
 					$o = $line[$i];
 					$b = $this->alpha[$line[$i + 1]];
 					$p = $this->switch_($a, $o , $b);
-					
-					$line[$i - 1] = $p;
+
+					if($chk !== '!')	
+						$line[$i - 1] = $p;
 					$line[$i] = ' ';
 					$line[$i + 1] = $p;
 				}	
@@ -133,19 +154,55 @@ class run extends expert
 		return ($p);	
 	}
 
+	private function solver_r($b , $a)
+	{
+		$i = 0;
+		$temp = $b;
+		while($temp[$i])
+		{
+			if($this->solver_($temp) !== $a)
+				$temp = $b;
+			else
+				return 0;
+			if ( $this->alpha[$temp[$i]] === 1)
+				$temp[$i] = '0';
+			else if ($this->alpha[$temp[$i]] === 0)
+				$temp[$i] = '1';
+			$i++;
+		}
+		$i = 0;
+		$k = 0;
+		while($b[$i])
+		{
+			if($this->alpha[$b[$i]] !== $temp[$i] 
+					&& ($temp[$i] === '1' or $temp[$i] === '0'))
+			{
+				if ($temp[$i] === '1')
+					$this->alpha[$b[$i]] = 1;
+				else
+					$this->alpha[$b[$i]] = 0;
+				$k++;
+			}
+			$i++;
+		}
+		return ($k);
+	}
+
 	protected function switch_($value1, $operator, $value2)
 	{
 		switch ($operator) 
 		{
-		case '+':
-			return $value1 & $value2;
-			break;
-		case '|':
-			return $value1 | $value2;
-			break;
-		case '^':
-			return $value1 ^ $value2;
-			break;
+			case '!':
+				return !$value2;
+			case '+':
+				return $value1 & $value2;
+				break;
+			case '|':
+				return $value1 | $value2;
+				break;
+			case '^':
+				return $value1 ^ $value2;
+				break;
 		}
 	}
 }
